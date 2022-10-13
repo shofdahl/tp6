9ffe16f07aa2bbf647f80397601d7575aa9d8bc5
shofdahl <shofdahl@gmail.com>
2022-10-13 07:35:54 +0000

    final changes update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,34 @@
-// JavaScript for TP5
+// JavaScript for TP6
+
+// function to load a file name from the URL "fromFile" into the object identified by "whereTo" //
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML = this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
+
+  loadFileInto("Ingredients.html", "#ingredients ul");
+  loadFileInto("equipment.html", "#equipment ul");
+  loadFileInto("directions.html", "#directions ol");
+
 
 window.onload = function() {
   
@@ -21,6 +51,7 @@ window.onload = function() {
   }
   
   document.querySelector("#recipeColumns #directions ol").innerHTML += "<li>Give to your friends for a sweet surprise.</li>";
+
 
   
 } //end window.onload
